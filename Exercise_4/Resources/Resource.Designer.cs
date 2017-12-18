e9c97d275c134938d73cb91edafee5f975a9742d
Mẫn <vanman.lqd@gmail.com>
2017-12-18 09:51:42 +0700

    add code

--- Exercise_4/Resources/Resource.Designer.cs
+++ Exercise_4/Resources/Resource.Designer.cs
@@ -69,53 +69,65 @@ namespace Exercise_4
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050015
+			public const int btnAdd = 2131034133;
+			
+			// aapt resource value: 0x7f050008
+			public const int btnC = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int btnCE = 2131034121;
+			
 			// aapt resource value: 0x7f050011
-			public const int btnAdd = 2131034129;
+			public const int btnChangeStatus = 2131034129;
 			
-			// aapt resource value: 0x7f050014
-			public const int btnDiv = 2131034132;
+			// aapt resource value: 0x7f05000a
+			public const int btnDel = 2131034122;
 			
-			// aapt resource value: 0x7f05000f
-			public const int btnDot = 2131034127;
+			// aapt resource value: 0x7f050018
+			public const int btnDiv = 2131034136;
+			
+			// aapt resource value: 0x7f050013
+			public const int btnDot = 2131034131;
 			
 			// aapt resource value: 0x7f050006
 			public const int btnEight = 2131034118;
 			
-			// aapt resource value: 0x7f050009
-			public const int btnFive = 2131034121;
+			// aapt resource value: 0x7f05000c
+			public const int btnFive = 2131034124;
 			
-			// aapt resource value: 0x7f05000a
-			public const int btnFour = 2131034122;
+			// aapt resource value: 0x7f05000b
+			public const int btnFour = 2131034123;
 			
-			// aapt resource value: 0x7f050013
-			public const int btnMul = 2131034131;
+			// aapt resource value: 0x7f050017
+			public const int btnMul = 2131034135;
 			
 			// aapt resource value: 0x7f050005
 			public const int btnNine = 2131034117;
 			
-			// aapt resource value: 0x7f05000d
-			public const int btnOne = 2131034125;
+			// aapt resource value: 0x7f05000e
+			public const int btnOne = 2131034126;
 			
 			// aapt resource value: 0x7f050007
 			public const int btnSeven = 2131034119;
 			
-			// aapt resource value: 0x7f050008
-			public const int btnSix = 2131034120;
+			// aapt resource value: 0x7f05000d
+			public const int btnSix = 2131034125;
 			
-			// aapt resource value: 0x7f050015
-			public const int btnSolve = 2131034133;
+			// aapt resource value: 0x7f050019
+			public const int btnSolve = 2131034137;
+			
+			// aapt resource value: 0x7f050016
+			public const int btnSub = 2131034134;
+			
+			// aapt resource value: 0x7f050010
+			public const int btnThree = 2131034128;
+			
+			// aapt resource value: 0x7f05000f
+			public const int btnTwo = 2131034127;
 			
 			// aapt resource value: 0x7f050012
-			public const int btnSub = 2131034130;
-			
-			// aapt resource value: 0x7f05000b
-			public const int btnThree = 2131034123;
-			
-			// aapt resource value: 0x7f05000c
-			public const int btnTwo = 2131034124;
-			
-			// aapt resource value: 0x7f05000e
-			public const int btnZero = 2131034126;
+			public const int btnZero = 2131034130;
 			
 			// aapt resource value: 0x7f050002
 			public const int linearLayout1 = 2131034114;
@@ -123,8 +135,8 @@ namespace Exercise_4
 			// aapt resource value: 0x7f050003
 			public const int linearLayout2 = 2131034115;
 			
-			// aapt resource value: 0x7f050010
-			public const int linearLayout3 = 2131034128;
+			// aapt resource value: 0x7f050014
+			public const int linearLayout3 = 2131034132;
 			
 			// aapt resource value: 0x7f050004
 			public const int linearLayout4 = 2131034116;
@@ -167,8 +179,20 @@ namespace Exercise_4
 			// aapt resource value: 0x7f040000
 			public const int app_name = 2130968576;
 			
+			// aapt resource value: 0x7f040012
+			public const int btnAC = 2130968594;
+			
 			// aapt resource value: 0x7f04000c
 			public const int btnAdd = 2130968588;
+			
+			// aapt resource value: 0x7f040011
+			public const int btnC = 2130968593;
+			
+			// aapt resource value: 0x7f040013
+			public const int btnChangeStatus = 2130968595;
+			
+			// aapt resource value: 0x7f040014
+			public const int btnDel = 2130968596;
 			
 			// aapt resource value: 0x7f04000e
 			public const int btnDiv = 2130968590;
